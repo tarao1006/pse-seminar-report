48d9f6317e6fddea0287cad33a6c0aa7022e06c7
tarao1006 <taiga4562@gmail.com>
2021-12-02 11:14:34 +0900

    first commit

---
+++ .latexmkrc
@@ -0,0 +1,5 @@
+$latex      = "find . -type f -name '*.tex' | xargs sed -i '' -e 's/、/，/g' -e 's/。/．/g'; platex -synctex=1 -halt-on-error %O %S";
+$bibtex     = "pbibtex %O %B";
+$dvipdf     = "dvipdfmx -f ptex-haranoaji.map -V 7 %O -o %D %S";
+$max_repeat = 5;
+$pdf_mode   = 3;
---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git config user.name github-actions
+git config user.email github-actions@github.com
+git fetch --unshallow
+git switch ${INPUT_DISTRIBUTION_BRANCH}
+git merge ${GITHUB_REF_NAME}
+COPY=false
+if [[ ! -f .latexmkrc ]]; then
+  COPY=true
+  cp /.latexmkrc ./.latexmkrc
+fi
+latexmk ${INPUT_PREFIX}/${GITHUB_REF_NAME}/${INPUT_REPORT_FILENAME} -output-directory=${INPUT_DISTRIBUTION_BRANCH}/${GITHUB_REF_NAME}
+if ${COPY}; then
+  rm .latexmkrc
+fi
+git add .
+git commit -m ":tada: Compiled ${GITHUB_REF_NAME}/${INPUT_REPORT_FILENAME}"
+git push
