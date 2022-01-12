3c33e2666ef8212924631a838210014659466d8a
tarao1006 <taiga4562@gmail.com>
2022-01-12 15:27:09 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,8 @@ else
   cp /.latexmkrc $HOME/.latexmkrc
 fi
 cp /main.py ./main.py
-python3 main.py
+python3 main.py --filename ${INPUT_REPORT_FILENAME} --target-directory ${INPUT_PREFIX}/${GITHUB_REF_NAME} --output-directory ${INPUT_DISTRIBUTION_BRANCH}/${GITHUB_REF_NAME}
+rm main.py
 
 git add ${INPUT_DISTRIBUTION_BRANCH}/${GITHUB_REF_NAME}
 git commit -m ":tada: Compiled ${GITHUB_REF_NAME}/${INPUT_REPORT_FILENAME} [${timestamp}]"
